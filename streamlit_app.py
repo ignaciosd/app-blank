a005658b37d36acc1c570ac65978c69ad25a42ec
ignaciosd <73096676+ignaciosd@users.noreply.github.com>
2024-08-30 07:55:31 -0400

    First Edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("It works !!!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+x = st.text_input('How old are you?')
+print(f'## {x} ... I am that old')
+
