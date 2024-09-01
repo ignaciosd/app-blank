0e7f995841edbd751c2a91b716d990ef3ad71d6c
ignaciosd <73096676+ignaciosd@users.noreply.github.com>
2024-09-01 11:21:41 -0400

    Text awesomeness

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if img_file_buffer is not None:
         time.sleep(0.01)
         my_bar.progress(percent_complete + 1, text=progress_text)
     time.sleep(1)
-    my_bar.progress(percent_complete + 1, text="AWESOMENESS level -> 100%")
+    my_bar.progress(percent_complete + 1, text="## AWESOMENESS level -> 100%")
     #my_bar.empty()
     st.snow()
     #st.button("Rerun")
