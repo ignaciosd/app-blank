41a2669df0b8ac229c24cc3004aae30dc6111f70
ignaciosd <73096676+ignaciosd@users.noreply.github.com>
2024-09-01 10:55:02 -0400

    Update streamlit_app.py
    Webcam Edit V2

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ from PIL import Image
 import numpy as np
 
 img_file_buffer = st.camera_input("Take a picture")
-st.write(img_file_buffer.shape)
+#st.write(img_file_buffer.shape)
 
 if img_file_buffer is not None:
     # To read image file buffer as a PIL Image:
